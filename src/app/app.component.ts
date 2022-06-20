c2f0fcf3d86551a2601cb57b4c1c593fe325ff18
Yves Mbulu-ntoto <ymbuluntoto@dawan.fr>
2022-06-20 11:20:31 +0200

    creating modules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { POKEMONS } from './mock-pokemon-list';
-import { Pokemon } from './pokemon';
-import { BorderCardDirective } from './border-card.directive';
+
 @Component({
   selector: 'app-root',
   templateUrl: `app.component.html`
